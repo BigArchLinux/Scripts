b2dc1382d4d050fe3add0255794e576d31d2418c
BigArchLinux <145774158+BigArchLinux@users.noreply.github.com>
2023-09-25 07:47:25 -0300

    Update limpeza-manjaro.sh

--- limpeza-manjaro.sh
+++ limpeza-manjaro.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Limpar cache de pacotes"
-sudo pacman -Sc --noconfirm && sudo pacman -Scc --noconfirm
+sudo pacman -Sc --noconfirm && sudo pacman -Scc --noconfirm && sudo pamac clean --no-confirm
 
 echo "Limpar pastas recentes"
 echo | tee $HOME/.local/share/recently-used*
