0c00adfcf763c86e256478fb4c6074530be5e4b4
BigArchLinux <145774158+BigArchLinux@users.noreply.github.com>
2023-10-17 17:51:19 -0300

    Update memoriaram.sh

--- memoriaram.sh
+++ memoriaram.sh
@@ -1,11 +1,18 @@
-echo "Exibindo o consumo da memória RAM"
-free -h
-echo "Limpando Cache da Memória RAM"
-echo 3 > /proc/sys/vm/drop_caches
+#!/bin/bash
+
+# Limpar Cache e Buffer
+sudo sync; echo 3 | sudo tee /proc/sys/vm/drop_caches
 sysctl -w vm.drop_caches=1
 sysctl -w vm.drop_caches=2
 sysctl -w vm.drop_caches=3
 clear
-echo "Limpeza do Cache da Memória RAM efetuada com sucesso"
-echo "Exibindo o consumo da mamória RAM"
+
+# Limpar a memória SWAP
+sudo swapoff -a
+sudo swapon -a
+
+# Exibir mensagem de conclusão
+echo "Memória RAM limpa com sucesso!"
+
+# Exibindo o consumo da mamória RAM
 free -h
