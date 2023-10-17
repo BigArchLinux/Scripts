30da3e31cf105ae9bfaa1cbbcc4da0252be23bb8
BigArchLinux <145774158+BigArchLinux@users.noreply.github.com>
2023-10-17 13:24:01 -0300

    Update limpeza.sh

--- limpeza.sh
+++ limpeza.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Limpar cache de pacotes
+# Limpar o cache do pacman
 sudo pacman -Sc --noconfirm && sudo pacman -Scc --noconfirm && sudo pamac clean --no-confirm
+
+# Limpar pacotes antigos
+sudo paccache -r
 
 # Limpar pastas recentes
 echo | tee $HOME/.local/share/recently-used*
@@ -9,4 +12,4 @@ echo | tee $HOME/.local/share/recently-used*
 # Limpar o cache do usuário
 rm -rf ~/.cache*
 
-echo "Limpeza concluída."
+echo "Limpeza concluída com sucesso."
