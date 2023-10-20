385632317fdd0ca160d2538d5b1bba3c355119b1
Linux-XP <145774158+Linux-XP@users.noreply.github.com>
2023-10-20 09:32:36 -0300

    Update limpeza.sh

--- limpeza.sh
+++ limpeza.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# Limpar o cache do pacman
-sudo pacman -Sc --noconfirm && sudo pacman -Scc --noconfirm && sudo pamac clean --no-confirm
+# Atualizar a lista de pacotes
+sudo apt-get update
 
-# Limpar pacotes antigos
-sudo paccache -r
+# Limpar pacotes desnecessários sem remover dependências
+sudo apt-get --purge autoremove
 
-# Apagar os arquivos recentes
+# Limpar o cache de pacotes
+sudo apt-get clean
+
+# Limpar cache de pacotes obsoletos
+sudo apt-get autoclean
+
+# Limpar Pastas Recentes
 echo | tee $HOME/.local/share/recently-used*
 
 # Limpar o cache do usuário
