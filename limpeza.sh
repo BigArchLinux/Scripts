3718b3c2eb52e2fa1246d19a1572050b9903c379
Linux-XP <145774158+Linux-XP@users.noreply.github.com>
2023-10-24 11:29:23 -0300

    Update limpeza.sh

--- limpeza.sh
+++ limpeza.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
-# Atualizar a lista de pacotes
-sudo apt-get update
-
-# Limpar pacotes desnecessários sem remover dependências
-sudo apt-get --purge autoremove
-
-# Limpar o cache de pacotes
+# Limpeza de cache e pacotes não utilizados
 sudo apt-get clean
-
-# Limpar cache de pacotes obsoletos
 sudo apt-get autoclean
+sudo apt-get autoremove
+
+# Limpeza de diretórios temporários
+sudo rm -rf /tmp/*
+sudo rm -rf /var/tmp/*
+
+# Limpeza de caches de aplicativos específicos (você pode adicionar mais conforme necessário)
+sudo rm -rf ~/.cache/thumbnails/* # Cache de miniaturas
+sudo rm -rf ~/.cache/mozilla/firefox/* # Cache do Firefox
+sudo rm -rf ~/.cache/google-chrome/* # Cache do Google Chrome
+sudo rm -rf ~/.cache/BraveSoftware/* # Cache do brave
+
+# Limpeza de logs antigos
+sudo find /var/log -type f -name "*.log" -exec truncate -s 0 {} \;
+
+# Limpeza do lixo do usuário
+rm -rf ~/.local/share/Trash/*
+rm -rf ~/.thumbnails/*
 
 # Limpar Pastas Recentes
 echo | tee $HOME/.local/share/recently-used*
 
-# Limpar o cache do usuário
-rm -rf ~/.cache*
+# Limpeza de histórico de comandos do terminal
+cat /dev/null > ~/.bash_history && history -c && exit
 
-echo "Limpeza concluída com sucesso."
+# Informar que a limpeza foi concluída
+echo "Limpeza concluída com sucesso!"
