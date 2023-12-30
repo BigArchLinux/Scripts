81222432abad1d11fe285dce9e47cda4c6ab2d7e
Linux-XP <145774158+Linux-XP@users.noreply.github.com>
2023-12-30 06:49:56 -0300

    Update limpeza-manjaro.sh

--- limpeza-manjaro.sh
+++ limpeza-manjaro.sh
@@ -40,6 +40,8 @@ cat /dev/null > ~/.bash_history && history -c && exit
 sudo sh -c 'echo 1 > /proc/sys/vm/drop_caches'
 sudo sh -c 'sync; echo 2 > /proc/sys/vm/drop_caches'
 sudo sh -c 'sync; echo 3 > /proc/sys/vm/drop_caches'
+sudo swapoff -a
+sudo swapon -a
 
 # Informar que a limpeza foi concluída
 echo "Limpeza concluída com sucesso!"
